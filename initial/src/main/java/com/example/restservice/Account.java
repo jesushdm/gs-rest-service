bdcc45011cad5d688c7943525e284c7b26de7a9b
jesushdm <33213507+jesushdm@users.noreply.github.com>
2020-02-24 22:36:13 +0100

    Change of the private fields.

--- initial/src/main/java/com/example/restservice/Account.java
+++ initial/src/main/java/com/example/restservice/Account.java
@@ -1,19 +1,43 @@
 package com.example.restservice;
 
+import java.util.Currency;
+
+import org.joda.money.Money;
+
 public class Account {
-	private  long id;
-	private  String content;
+	String name;
+	Currency currency;
+	Money balance;
+	Boolean treasury;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Money getBalance() {
+		return balance;
+	}
+	public void setBalance(Money balance) {
+		this.balance = balance;
+	}
+	public Boolean getTreasury() {
+		return treasury;
+	}
+	public void setTreasury(Boolean treasury) {
+		this.treasury = treasury;
+	}
+	
 	
 	//constructor
-	public Account(long id, String content) {
-		this.id = id;
-		this.content = content;
+	public Account(String name, 	Currency currency,  Money balance, 	Boolean treasury ) {
+		this.name = name;
+		this.currency = currency;
+		this.balance = balance;
+		this.treasury = true;
 	}
-	public long getId() {
-		return id;
-	}
-	public String getContent() {
-		return content;
-	}
+
 	
 }
